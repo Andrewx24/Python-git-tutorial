8c0c7bf327b199bb559a06b6a9626965e63aa5fb
Andrew Aliaj <andyaliaj@gmail.com>
2024-06-05 07:14:54 -0400

    new branch

--- new.py
+++ new.py
@@ -2,5 +2,8 @@ age = int( input("Enter your age: ") )
  
 if age < 18:
          print("You are a minor")
-else:
+else age >= 18:
          print("You are an adult")
+
+elif age >= 65:
+         print("You are a senior citizen")
